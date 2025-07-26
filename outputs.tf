441923ae1510e3925abf5c14d06d88a04107dad4
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-26 11:10:04 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-  GNU nano 7.2                                                                                                                        outputs.tf
 output "public_ip" {
   value       = aws_instance.strapi_ec2.public_ip
   description = "Use this to access Strapi in browser: http://<public_ip>"
