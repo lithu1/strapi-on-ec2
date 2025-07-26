56cfe88c0785454718a197a1b7124827be5836a3
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-26 19:04:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,54 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg"
+  description = "Allow SSH and Strapi port"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "strapi" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.strapi_sg.id]
+
+  tags = {
+    Name = "strapi-server"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+              docker pull lithu213/strapi-app:latest
+              docker run -d -p 1337:1337 --name strapi-app lithu213/strapi-app:latest
+              EOF
+}
+
 output "public_ip" {
   value = aws_instance.strapi.public_ip
 }
